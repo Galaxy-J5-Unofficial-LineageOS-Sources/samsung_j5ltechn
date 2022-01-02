8547205f2f6684587d3f04cbae84670542834a97
David <davidmolinamorales1999@gmail.com>
2022-01-02 02:19:44 +0100

    j5ltechn: Remove duplicated call

--- lineage_j5ltechn.mk
+++ lineage_j5ltechn.mk
@@ -7,8 +7,6 @@
 # Inherit from common
 $(call inherit-product, device/samsung/j5-common/lineage.mk)
 
-$(call inherit-product, device/samsung/j5ltechn/device.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5ltechn
 PRODUCT_NAME := lineage_j5ltechn
